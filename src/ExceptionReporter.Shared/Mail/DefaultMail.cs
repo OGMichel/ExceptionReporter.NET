04ff22ccd5219c6cbe2162652a73417e18aeb2c4
OLPHE-GALLIARD Michel <michel.olphegalliard@addactis.com>
2022-02-03 10:46:32 +0100

    Add comment on mailto limitation

--- src/ExceptionReporter.Shared/Mail/DefaultMail.cs
+++ src/ExceptionReporter.Shared/Mail/DefaultMail.cs
@@ -19,6 +19,7 @@ namespace ExceptionReporting.Mail
 
 	public void Send(string subject, string body)
 	{
+	  // mailto does not support attachment, we are looking for a way to support it, but we don't have the solution yet
 	  string mailto = string.Format("mailto:{0}?Subject={1}&Body={2}", _recipient, subject, Uri.EscapeDataString(body));
 	  Process.Start(new ProcessStartInfo(mailto) { UseShellExecute = true });
 	}
